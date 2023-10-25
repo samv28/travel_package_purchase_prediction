bf54df6849ef1c22761fd6d8396fd92387b3d3aa
Sameer Khatwani <75417490+samv28@users.noreply.github.com>
2023-10-25 19:46:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from streamlit_option_menu import option_menu
 import  Prediction, contact,Introduction
 
 
-st.set_page_config(
-    page_title="Travel Package Purchase Prediction",
-    page_icon="🪖",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+
 
 class MultiApp:
 
