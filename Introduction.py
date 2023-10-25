726798de8cccaa43408724570bc90b63701fc290
Sameer Khatwani <75417490+samv28@users.noreply.github.com>
2023-10-26 00:29:11 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st 
 import main
 def app():
@@ -10,5 +8,3 @@ def app():
     
     st.write("The goal of the project is to create a data-driven prediction engine that can accurately anticipate , if the potential clients are likely to buy trip package  potential . The purpose of this classification model is to increase customer happiness, optimize operational procedures, and personalize travel packages to meet the needs of different travelers.  ")
     st.write("The travel business may increase income and acquire a competitive edge in the market by utilizing insights from consumer demographics, historical preferences, seasonal patterns, and marketing data.")
-
-    
