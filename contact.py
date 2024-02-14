6e1d02d93ad0cfbb953bfadca51855bdd0c1f348
Sameer Khatwani <75417490+samv28@users.noreply.github.com>
2024-02-14 15:54:04 +0530

    Update contact.py

--- contact.py
+++ contact.py
@@ -5,4 +5,4 @@ import streamlit as st
 def app():
     st.markdown("# Contact us:")
     st.markdown('[Riya Shukla](https://github.com/riya0785) , [Sahil Goyal](https://github.com/sahilgoyal7214),[Sameer Khatwani](https://github.com/samv28)')
-    st.markdown('Contact via mail: [sameerkhatwani10@gmail.com]')
+    st.markdown('Contact via mail: [sameerkhatwani10@gmail.com],[riyashukla107@gmail.com]')
